c7954d3c68d18cbd51eacf86d704111ff57b76e0
Courdevelops <courdevelops@gmail.com>
2022-04-15 16:57:09 -0400

    testimonials

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,7 +2,7 @@ import {React} from 'react';
 import '../App.css';
 
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { Navbar, Nav, Container, Badge } from 'react-bootstrap';
+import { Navbar, Nav, Container, Badge, Accordion } from 'react-bootstrap';
 
 
 function Home(){
@@ -35,7 +35,7 @@ function Home(){
 
                 {/* PHOTO */}
                 <div className='piccy'>
-                    <img src='https://courdevelops.com/media/avatar/ProfilePic.JPG' width={300} className='img-fluid rounded-circle' alt='Me'></img>
+                    <img src='https://courdevelops.com/media/avatar/ProfilePic.JPG' width={300} height={200} className='img-fluid rounded-circle' alt='Me'></img>
                 </div>
 
 
@@ -65,21 +65,56 @@ function Home(){
                         <h4>Communicator</h4>
                     </div>
                     <div className='skill-item3'>
-                        <img src="images/customerfocused.png" height={125} width={120} alt="start"></img>
+                        <img src="images/customerfocused.png" height={125} width={115} alt="start"></img>
                         <h4>User Experience Focused</h4>
                     </div>
                 </div>
 
                 {/* PORTFOLIO */}
                 <div className='portfolio'>
-                    <h1 id='portfoliotitle'>Portfolio</h1>
+                    <h1 id='portfoliotitle'>Portfolio</h1> 
+                    <br></br>
                     <div className='jobs'>
-                        <h2>Project #1</h2>
-                        <p>This is the description of project #1.</p>
+                        <Accordion>
+                            <Accordion.Item id="accordy" eventKey="0">
+                                <Accordion.Header><h3 id='accordyfont'>Project #1</h3></Accordion.Header>
+                                <Accordion.Body>
+                                <p>The Description of Project #1</p>
+                                <a href="w3c.com">The Link to Project #1</a>
+                                </Accordion.Body>
+                            </Accordion.Item>
+                            <br></br>
+                            <Accordion.Item id="accordy" eventKey="1">
+                                <Accordion.Header><h3 id='accordyfont'>Project #2</h3></Accordion.Header>
+                                <Accordion.Body>
+                                <p>The Description of Project #2</p>
+                                <a href="w3c.com">The Link to Project #2</a>
+                                </Accordion.Body>
+                            </Accordion.Item>
+                        </Accordion>
                     </div>
                 </div>
-
+                <br></br><br></br>
                 {/* TESTIMONIALS */}
+                <div className='testimonials'>
+                    <h1 id='testimonialstitle'>Testimonials</h1>
+                    <div className='testimonial-item1'>
+                        <img src="images/customerfocused.png" height={100} width={100} alt="start"></img>
+                        <h4>Testimonial #1</h4>
+                        <p>"This Programmer is Great. I will hire him again"</p>
+                    </div>
+                    <div className='testimonial-item2'>
+                        <img src="images/customerfocused.png" height={100} width={100} alt="start"></img>
+                        <h4>Testimonial #2</h4>
+                        <p>"This Programmer is Great. I will hire him again"</p>
+                    </div>
+                    <div className='testimonial-item3'>
+                        <img src="images/customerfocused.png" height={100} width={100} alt="start"></img>
+                        <h4>Testimonial #3</h4>
+                        <p>"This Programmer is Great. I will hire him again"</p>
+                    </div>
+                    
+                </div>
   
         </Container>
      </>
