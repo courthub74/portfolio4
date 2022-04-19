f1eea3428ba66627c30e7d3147e6a0b360f06405
Courdevelops <courdevelops@gmail.com>
2022-04-18 20:12:55 -0400

    herokupush

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -22,16 +22,19 @@ function Portfolio() {
                     <div id="ports">
                         <h3 className='projecttitle'>Project #1</h3>
                             <p>The Description of Project #1</p>
+                            <p>Tech involved: one | two | three | four</p>
                             <a target="blank" href="https://w3c.com">The Link to Project #1</a>
                             <br></br><hr></hr>
                             
                         <h3 className='projecttitle'>Project #2</h3>
                             <p>The Description of Project #2</p>
+                            <p>Tech involved: one | two | three | four</p>
                             <a target="blank" href="https://w3c.com">The Link to Project #2</a>
                             <br></br><hr></hr>
 
                         <h3 className='projecttitle'>Project #3</h3>
                             <p>The Description of Project #3</p>
+                            <p>Tech involved: one | two | three | four</p>
                             <a target="blank" href="https://w3c.com">The Link to Project #3</a>
                     </div>
             </div>
